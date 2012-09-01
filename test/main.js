c00442088359c5cacdf430142454bdbef1bbf13d
Bert Pareyn <pareyn.bert@gmail.com>
2012-09-01 13:58:02 +0100

    Add forms in config

--- test/main.js
+++ test/main.js
@@ -2,35 +2,44 @@ var should = require('should');
 var adminui = require('../lib/main');
 
 adminui.generateAdminUI({
-    "rows": [{
-            "element": "header",
-            "type": "h1",
-            "label": "Create a new tenant"
-        }, {
-            "element": "label",
-            "label": "ID:",
-            "for": "tenant_id"
-        }, {
-            "element": "input",
-            "id": "tenant_id"
-        }, {
-            "element": "label",
-            "label": "Name:",
-            "for": "tenant_name"
-        }, {
-            "element": "input",
-            "id": "tenant_name"
-        }, {
-            "element": "label",
-            "label": "Port:",
-            "for": "tenant_port"
-        }, {
-            "element": "input",
-            "id": "tenant_port"
-        }, {
-            "element": "button",
-            "type": "submit",
-            "label": "Create tenant"
+    "rows": [
+        {
+            "element": "form",
+            "action": "/create",
+            "method": "post",
+            "elements": [{
+                "element": "header",
+                "type": "h1",
+                "label": "Create a new tenant"
+            }, {
+                "element": "label",
+                "label": "ID:",
+                "for": "tenant_id"
+            }, {
+                "element": "input",
+                "id": "tenant_id",
+                "name": "id"
+            }, {
+                "element": "label",
+                "label": "Name:",
+                "for": "tenant_name"
+            }, {
+                "element": "input",
+                "id": "tenant_name",
+                "name": "name"
+            }, {
+                "element": "label",
+                "label": "Port:",
+                "for": "tenant_port"
+            }, {
+                "element": "input",
+                "id": "tenant_port",
+                "name": "port"
+            }, {
+                "element": "button",
+                "type": "submit",
+                "label": "Create tenant"
+            }]
         }
     ]
 }, function(html) {
